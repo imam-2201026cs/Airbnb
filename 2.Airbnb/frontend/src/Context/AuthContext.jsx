ae6ad9574652e34e0eff299d7f6e1eb7484c244b
imam-2201026cs <ihk748284@gmail.com>
2025-07-26 23:43:35 +0530

    Update AuthContext.jsx

--- 2.Airbnb/frontend/src/Context/AuthContext.jsx
+++ 2.Airbnb/frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    const serverUrl = "hwerhjkjk"
+    const serverUrl = "https://media-irc8.onrender.com"
 
     let [loading,setLoading]=useState(false)
 
